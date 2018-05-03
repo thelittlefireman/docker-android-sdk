fb8d7b0351dc33dbffd88a5317e00a663382f457
Thomas Goureau <thomas.goureau@mythalesgroup.io>
2018-05-03 13:53:28 +0200

    add auto exit and fix build tool version

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ index=0
 for i in "${android_version[@]}"
 do
    echo "$i"
-   docker build -t "thelittlefireman/docker-android-sdk:$i" --build-arg ANDROID_VERSION=$i --build-arg ANDROID_BUILD_TOOLS_VERSION=${android_build_tool_version[i]} .
+   docker build -t "thelittlefireman/docker-android-sdk:$i" --build-arg ANDROID_VERSION=$i --build-arg ANDROID_BUILD_TOOLS_VERSION=${android_build_tool_version[$index]} .
    docker push "thelittlefireman/docker-android-sdk:$i"
    index=$((index + 1))
 done
